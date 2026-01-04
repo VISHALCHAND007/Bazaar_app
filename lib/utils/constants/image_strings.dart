44a338699649301ba1e1d06ecd766ba48760f99c
Vishal Chand <sam.chand96@gmail.com>
2026-01-04 22:59:06 +0530

    Defined the app constants.

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,12 @@
+class ImageStrings {
+  ImageStrings._();
+
+
+  //App icon's
+  static const String appIconLight = "assets/logos/light_icon.png";
+  static const String appIconDark = "assets/logos/dark_icon.png";
+
+  //Social media images
+  static const String google = "assets/logos/google.png";
+  static const String facebook = "assets/logos/facebook.png";
+}
