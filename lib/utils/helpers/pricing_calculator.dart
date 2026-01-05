05e6575f073701aec928a2ba4525b632e5e75a61
Vishal Chand <sam.chand96@gmail.com>
2026-01-05 15:50:43 +0530

    Completed add all the utility classes for now.

--- lib/utils/helpers/pricing_calculator.dart
+++ lib/utils/helpers/pricing_calculator.dart
@@ -1,5 +1,34 @@
 class PricingCalculator {
   PricingCalculator._();
 
+  //Calculate price based on tax and shipping
+  static double calculateTotalPrice(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
 
+    double shippingCost = getShippingCost(location);
+
+    return productPrice + taxAmount + shippingCost;
+  }
+
+  static String calculateShippingCost(double productPrice, String location) {
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  static String calculateTax(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location) {
+    return .10; // example value
+  }
+
+  static double getShippingCost(String location) {
+    return 5.00;
+  }
 }
+
---
+++ lib/utils/validators/validator.dart
@@ -0,0 +1,48 @@
+class Validator {
+  Validator._();
+  
+  static String? validateEmail(String? value) {
+    if(value == null || value.isEmpty) {
+      return "Email is required";
+    }
+    final emailRegex = RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+
+    if(!emailRegex.hasMatch(value)) {
+      return "Email email address";
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if(value == null || value.isEmpty){
+      return "Password si required";
+    }
+    if(value.length < 6) {
+      return "Password must be atleast 6 characters long.";
+    }
+    //checks for upper case letters
+    if(!value.contains(RegExp(r"[A-Z]"))) {
+      return "Password must contain at least one uppercase letter.";
+    }
+    //checks for numbers
+    if(!value.contains(RegExp(r"0-9"))) {
+      return "Password must contain at least one number.";
+    }
+    //checks for special characters
+    if(!value.contains(RegExp(r'[!@#$%^&*()<>?":{}|<>]'))) {
+      return "Password must contain at least one special character.";
+    }
+    return null;
+  }
+
+  static String? validatePhoneNumber(String? value) {
+    if(value == null || value.isEmpty) {
+      return "Phone number is required.";
+    }
+    final phoneRegExp = RegExp(r'^\d{10}$');
+    if(!phoneRegExp.hasMatch(value)) {
+      return "Invalid phone number format (10 digits required)";
+    }
+    return null;
+  }
+}
