38bfeaacfc29aa64a60211c1db1a13fab9bc4141
Vishal Chand <sam.chand96@gmail.com>
2026-02-01 20:29:33 +0530

    Implemented the double back press to exit feature.

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -7,66 +7,86 @@ import 'package:e_commerce/features/shop/screens/home/widgets/home_app_bar.dart'
 import 'package:e_commerce/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:e_commerce/utils/constants/custom_size.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import '../../../../common/widgets/customShapes/containers/custom_header_container.dart';
 import '../../../../common/widgets/customShapes/containers/search_container.dart';
 import '../../../../utils/constants/image_strings.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? _lastBackPressed;
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          //header
-          const PrimaryHeaderContainer(
-            child: Column(
-              children: [
-                // App bar
-                HomeAppbar(),
-                SizedBox(height: CustomSize.spaceBtwSections),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (_, didPop) {
+        final now = DateTime.now();
+        if(_lastBackPressed == null || now.difference(_lastBackPressed!) > const Duration(seconds: 2) ) {
+          _lastBackPressed = now;
+          Get.snackbar("Exit", "Press back again to exit.", snackPosition: .BOTTOM, margin: const .all(10));
+        } else {
+          SystemNavigator.pop();
+        }
+      },
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            //header
+            const PrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  // App bar
+                  HomeAppbar(),
+                  SizedBox(height: CustomSize.spaceBtwSections),
 
-                //Search bar
-                SearchContainer(text: "Search in store", showBackground: true),
-                SizedBox(height: CustomSize.spaceBtwSections),
+                  //Search bar
+                  SearchContainer(text: "Search in store", showBackground: true),
+                  SizedBox(height: CustomSize.spaceBtwSections),
 
-                //Categories
-                HomeCategories(),
+                  //Categories
+                  HomeCategories(),
 
-                SizedBox(height: CustomSize.spaceBtwSections),
-              ],
+                  SizedBox(height: CustomSize.spaceBtwSections),
+                ],
+              ),
             ),
-          ),
-          //body::
+            //body::
 
-          //carousel
-          Padding(
-            padding: const .all(CustomSize.defaultSpace),
-            child: Column(
-              children: [
-                const BannerSlider(
-                  banners: [
-                    ImageStrings.banner1,
-                    ImageStrings.banner2,
-                    ImageStrings.banner3,
-                  ],
-                ),
-                //popular products
-                SectionHeading(title: "Popular Products", onPressed: () => Get.to(() => const AllProducts())),
-                const SizedBox(height: CustomSize.spaceBtwItems),
+            //carousel
+            Padding(
+              padding: const .all(CustomSize.defaultSpace),
+              child: Column(
+                children: [
+                  const BannerSlider(
+                    banners: [
+                      ImageStrings.banner1,
+                      ImageStrings.banner2,
+                      ImageStrings.banner3,
+                    ],
+                  ),
+                  //popular products
+                  SectionHeading(title: "Popular Products", onPressed: () => Get.to(() => const AllProducts())),
+                  const SizedBox(height: CustomSize.spaceBtwItems),
 
-                GridLayout(
-                  itemCount: 2,
-                  itemBuilder: (BuildContext _, int index) =>
-                      const ProductsCardVertical(),
-                ),
-              ],
+                  GridLayout(
+                    itemCount: 2,
+                    itemBuilder: (BuildContext _, int index) =>
+                        const ProductsCardVertical(),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
